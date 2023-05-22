7c0487be12503558a825396f5eac5f8c05cde41c
Mitsufiro <lyashenk.ilya@gmail.com>
2023-05-22 13:53:30 +0300

    Proj load

--- routers/auth.py
+++ routers/auth.py
@@ -16,17 +16,15 @@ from schema import (CreateUserReq, ExpireTokenReq, LoginByEmailReq,
                     UpdateUserAdminReq, UpdateUserReq)
 from utils.jwt_service import jwt_service
 
-load_dotenv('.env')
+load_dotenv(".env")
 
 ACCESS_TOKEN_EXPIRE_MINUTES = 30  # 30 minutes
 REFRESH_TOKEN_EXPIRE_MINUTES = 60 * 24 * 7  # 7 days
-ALGORITHM = os.environ['ALGORITHM']
-JWT_SECRET_KEY = os.environ['JWT_SECRET_KEY']
-JWT_REFRESH_SECRET_KEY = os.environ['JWT_REFRESH_SECRET_KEY']
+ALGORITHM = os.environ["ALGORITHM"]
+JWT_SECRET_KEY = os.environ["JWT_SECRET_KEY"]
+JWT_REFRESH_SECRET_KEY = os.environ["JWT_REFRESH_SECRET_KEY"]
 
-ROUTER = APIRouter(
-    prefix="/auth",
-    tags=["User"])
+ROUTER = APIRouter(prefix="/auth", tags=["User"])
 
 from crud.user import CRUDUser
 from depedences.cruds import get_token_crud, get_users_crud
@@ -38,16 +36,16 @@ from depedences.cruds import get_token_crud, get_users_crud
     summary="Create user",
 )
 async def create_user(
-        new_user: CreateUserReq,
-        user_crud: CRUDUser = Depends(get_users_crud)
-        # token: TokenData = Depends(
-        #     RequiredRoles(
-        #         [
-        #             UserRole.admin,
-        #             UserRole.user
-        #         ]
-        #     )
-        # )
+    new_user: CreateUserReq,
+    user_crud: CRUDUser = Depends(get_users_crud)
+    # token: TokenData = Depends(
+    #     RequiredRoles(
+    #         [
+    #             UserRole.admin,
+    #             UserRole.user
+    #         ]
+    #     )
+    # )
 ):
     if await user_crud.check_email_user_exists(email=new_user.email):
         raise HTTPException(
@@ -64,8 +62,9 @@ async def create_user(
     response_model=TokensResp,
 )
 async def login_by_email(
-        req: LoginByEmailReq,
-        user_crud: CRUDUser = Depends(get_users_crud), token: CRUDToken = Depends(get_token_crud)
+    req: LoginByEmailReq,
+    user_crud: CRUDUser = Depends(get_users_crud),
+    token: CRUDToken = Depends(get_token_crud),
 ):
     """
     API method to authorize by email and password.
@@ -91,6 +90,7 @@ class RefreshTokenReq(BaseModel):
     """
     Model containing refresh token data.
     """
+
     token: str
 
 
@@ -99,11 +99,12 @@ class RefreshTokenReq(BaseModel):
     status_code=status.HTTP_200_OK,
     summary="Refresh token",
     description="Returns a new pair of access and "
-                "refresh tokens if refresh token is valid",
+    "refresh tokens if refresh token is valid",
     response_model=TokensResp,
 )
 async def refresh(
-        req: RefreshTokenReq,
+    req: RefreshTokenReq,
+    token: TokenData = Depends(RequiredRoles([UserRole.admin, UserRole.user])),
 ):
     """
     API method to refresh access and refresh tokens by provided refresh token.
@@ -122,13 +123,22 @@ async def refresh(
     )
 
 
-@ROUTER.post('/logout',summary='Logout form service')
-async def logout(token_data: TokenData = Depends(
-    RequiredRoles([UserRole.admin, UserRole.manager, UserRole.user, ])),
-        token: str = Depends(get_raw_token),
-        token_crud: CRUDToken = Depends(get_token_crud)):
+@ROUTER.post("/logout", summary="Logout form service")
+async def logout(
+    token_data: TokenData = Depends(
+        RequiredRoles(
+            [
+                UserRole.admin,
+                UserRole.manager,
+                UserRole.user,
+            ]
+        )
+    ),
+    token: str = Depends(get_raw_token),
+    token_crud: CRUDToken = Depends(get_token_crud),
+):
     """
-        API method that allows to add token to black list.
+    API method that allows to add token to black list.
     """
     logout = ExpireTokenReq(token=token, user_id=token_data.user_id)
     tokenbl = await token_crud.create_tokenbl(obj_in=logout)
@@ -142,16 +152,16 @@ async def logout(token_data: TokenData = Depends(
     response_model=UserBase,
 )
 async def get_current_user(
-        token: TokenData = Depends(
-            RequiredRoles(
-                [
-                    UserRole.admin,
-                    UserRole.manager,
-                    UserRole.user,
-                ]
-            )
-        ),
-        user_crud: CRUDUser = Depends(get_users_crud),
+    token: TokenData = Depends(
+        RequiredRoles(
+            [
+                UserRole.admin,
+                UserRole.manager,
+                UserRole.user,
+            ]
+        )
+    ),
+    user_crud: CRUDUser = Depends(get_users_crud),
 ) -> UserBase:
     """
     API method to get current User information.
@@ -165,18 +175,22 @@ async def get_current_user(
     return user
 
 
-@ROUTER.get('/get_all_users', response_model=List[EditableUserBase], summary='Get all users on pages')
+@ROUTER.get(
+    "/get_all_users",
+    response_model=List[EditableUserBase],
+    summary="Get all users on pages",
+)
 async def get_all_users(
-        token: TokenData = Depends(
-            RequiredRoles(
-                [
-                    UserRole.admin,
-                    UserRole.manager,
-                    UserRole.user,
-                ]
-            )
-        ),
-        users_crud: CRUDUser = Depends(get_users_crud),
+    token: TokenData = Depends(
+        RequiredRoles(
+            [
+                UserRole.admin,
+                UserRole.manager,
+                UserRole.user,
+            ]
+        )
+    ),
+    users_crud: CRUDUser = Depends(get_users_crud),
 ):
     users = await users_crud.get_all_users()
     return users
@@ -192,15 +206,15 @@ from fastapi_pagination import Page, Params
     response_model=User,
 )
 async def get_user_by_id(
-        user_id: UUID,
-        token: TokenData = Depends(
-            RequiredRoles(
-                [
-                    UserRole.admin,
-                ]
-            )
-        ),
-        user_crud: CRUDUser = Depends(get_users_crud),
+    user_id: UUID,
+    token: TokenData = Depends(
+        RequiredRoles(
+            [
+                UserRole.admin,
+            ]
+        )
+    ),
+    user_crud: CRUDUser = Depends(get_users_crud),
 ) -> UserBase:
     user = await user_crud.get(id=user_id)
     return user
@@ -213,15 +227,9 @@ async def get_user_by_id(
     response_model=Page[DBUser],
 )
 async def get_user_list(
-        token: TokenData = Depends(
-            RequiredRoles(
-                [
-                    UserRole.admin
-                ]
-            )
-        ),
-        params: Params = Depends(),
-        user_crud: CRUDUser = Depends(get_users_crud),
+    token: TokenData = Depends(RequiredRoles([UserRole.admin])),
+    params: Params = Depends(),
+    user_crud: CRUDUser = Depends(get_users_crud),
 ) -> Page[DBUser]:
     """
     API method to get list of all Users.
@@ -231,7 +239,7 @@ async def get_user_list(
     return users
 
 
-@ROUTER.get('/get_user', summary='Get user by email')
+@ROUTER.get("/get_user", summary="Get user by email")
 async def get_user(email, user_crud: CRUDUser = Depends(get_users_crud)):
     user = await user_crud.get_by_login(email=email)
     return user.is_active
@@ -248,15 +256,17 @@ from depedences.cruds import get_users_crud
     response_model=User,
 )
 async def edit_user_by_id(
-        user_id: UUID,
-        update_user_data: UpdateUserAdminReq,
-        token: TokenData = Depends(
-            RequiredRoles(
-                [
-                    UserRole.admin,
-                ]
-            )
-        ), user_crud: CRUDUser = Depends(get_users_crud)):
+    user_id: UUID,
+    update_user_data: UpdateUserAdminReq,
+    token: TokenData = Depends(
+        RequiredRoles(
+            [
+                UserRole.admin,
+            ]
+        )
+    ),
+    user_crud: CRUDUser = Depends(get_users_crud),
+):
     user = await user_crud.update(obj_id=user_id, obj_new=update_user_data)
     return user
 
@@ -268,22 +278,27 @@ async def edit_user_by_id(
     response_model=User,
 )
 async def edit_current_user(
-        update_user_data: UpdateUserReq,
-        token: TokenData = Depends(
-            RequiredRoles(
-                [
-                    UserRole.admin,
-                    UserRole.manager,
-                    UserRole.user,
-                ]
-            )
-        ), user_crud: CRUDUser = Depends(get_users_crud)):
+    update_user_data: UpdateUserReq,
+    token: TokenData = Depends(
+        RequiredRoles(
+            [
+                UserRole.admin,
+                UserRole.manager,
+                UserRole.user,
+            ]
+        )
+    ),
+    user_crud: CRUDUser = Depends(get_users_crud),
+):
     user = await user_crud.update(obj_id=token.user_id, obj_new=update_user_data)
     return user
 
 
-@ROUTER.delete('/private/user/delete', summary='Delete user by id')
-async def delete_user(user_id: UUID, token: TokenData = Depends(RequiredRoles([UserRole.admin])),
-                      user_crud: CRUDUser = Depends(get_users_crud)):
+@ROUTER.delete("/private/user/delete", summary="Delete user by id")
+async def delete_user(
+    user_id: UUID,
+    token: TokenData = Depends(RequiredRoles([UserRole.admin])),
+    user_crud: CRUDUser = Depends(get_users_crud),
+):
     user = await user_crud.del_user(user_id=user_id)
-    return 'User deleted'
+    return "User deleted"
